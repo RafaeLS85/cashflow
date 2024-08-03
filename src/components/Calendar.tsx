9168c4b58118da1301478850d0f2b3f58085a85f
Rafael Olivay <rafaelsebastian@live.com.ar>
2024-08-02 22:16:30 -0300

    add basic deps

---
+++ src/components/Calendar.tsx
@@ -0,0 +1,20 @@
+"use client";
+import 'react-calendar/dist/Calendar.css';
+import { useState } from 'react';
+import Calendar from 'react-calendar';
+
+type ValuePiece = Date | null;
+
+type Value = ValuePiece | [ValuePiece, ValuePiece];
+
+function CalendarComponent() {
+  const [value, onChange] = useState<Value>(new Date());
+
+  return (
+    <div>
+      <Calendar onChange={onChange} value={value} />
+    </div>
+  );
+}
+
+export default CalendarComponent;
